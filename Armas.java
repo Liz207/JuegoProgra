765589188122c35697ab23faf5f475bb4af3031f
YaDer423 <yaderisith4@gmail.com>
2025-08-05 18:52:30 -0600

    Add files via upload

---
+++ Armas.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package videojuego.entidades;
+
+/**
+ *
+ * @author Yader
+ */
+public class Armas {
+    private String nombre;
+    private String tipo;
+    private  int danioMinimo;
+    private   int  danioMaximo;
+    private String modificadores; 
+    
+    public Armas(String nombre, String tipo, int danioMinimo, int danioMaximo, String modificadores){
+    
+   this.nombre = nombre;
+   this.tipo = tipo;
+   this.danioMinimo = danioMinimo;
+   this.danioMaximo = danioMaximo;
+   this.modificadores = modificadores;
+    }
+   
+// Getter 
+
+public String getNombre() { return nombre; }
+public String getTipo() { return tipo; }
+public  int getDanioMinimo() { return danioMinimo; }
+public  int getDanioMaximo() { return danioMaximo; }
+public  String getModificadores() { return modificadores; }
+
+}
